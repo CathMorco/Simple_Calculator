e8e2a7243a55f87cf6fa48c31824493684ed868f
unknown <morco.cathleenjan.12192003@gmail.com>
2023-05-08 21:20:33 +0800

    Import necessary libraries and elements, establish pseudocodes

---
+++ Simple_Calculator_Pyqt5.py
@@ -0,0 +1,20 @@
+#Imports necessary elements
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+#creates class for widgets
+#creates function for GUI
+# Set window properties
+#Create a function for the selected operation
+#create function for calculate button
+# Get user inputs from QLineEdits
+#If input is not float, displays error message and clears initial input
+#if operator is not selected, prompts user to choose an operator
+# Calculate result based on selected operation
+# Display result
+#Asks user if they want to try again
+#If yes, repeats process
+#If no, displays thank you message and closes program
+#runs the main program
